bbb079ff7180389b05d6b73bef15556fb0d7cd6d
Tejal Mehta <tejal.mehta@city.ac.uk>
2023-04-06 11:35:15 -0700

    Update market_segment.py

--- market_segment.py
+++ market_segment.py
@@ -32,9 +32,9 @@ def main():
     
    
     State = st.text_input("Type In The State",type='default')
-    Market = st.radio ( "Select Market", ('East', 'West','South','Central'),required=True)
-    Market_Size = st.radio ( "Select Market Size", ('Small Market', 'Major Market'),required=True)
-    Product_Type = st.radio ( "Select Product Type", ("Coffee","Tea","Espresso","Herbal Tea"),required=True )
+    Market = st.radio ( "Select Market", ('East', 'West','South','Central'))
+    Market_Size = st.radio ( "Select Market Size", ('Small Market', 'Major Market'))
+    Product_Type = st.radio ( "Select Product Type", ("Coffee","Tea","Espresso","Herbal Tea"))
     Type = st.radio ( "Please Select The Type", ('Regular', 'Decaf'),required=True )
     
     
