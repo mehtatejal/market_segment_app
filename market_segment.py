3b380441cd4829e1434116007cd79a04e5bbe1af
Tejal Mehta <tejal.mehta@city.ac.uk>
2023-04-06 11:22:50 -0700

    Update market_segment.py

--- market_segment.py
+++ market_segment.py
@@ -13,8 +13,6 @@ st.subheader("Hello!    Welcome to Product Recommendation")
 image = Image.open("MarketSegment.png")
 st.image(image,use_column_width = True)
 
-# Set the theme of the app to 'dark'
-st.set_theme('dark')
 
 #load the model
 # classifier = pickle.load(open('final_pipeline.pkl','rb'))
